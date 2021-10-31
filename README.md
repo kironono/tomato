ed2176a2c83f33283f4b1301c2034982c2dde006
Junichi Kakisako <kilo.sw@gmail.com>
2021-10-31 13:13:13 +0900

    Customize stage time

--- README.md
+++ README.md
@@ -14,6 +14,22 @@ cargo install:
 cargo install tomato
 ```
 
+## Usage
+
+```
+USAGE:
+    tomato [OPTIONS]
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+    -l, --long-break-time <long_break_time>      Duration of long break time (minutes) [default: 15]
+    -s, --short-break-time <short_break_time>    Duration of short break time (minutes) [default: 5]
+    -w, --work-time <work_time>                  Duration of work time (minutes) [default: 25]
+```
+
 ## License
 
 `tomato` is distributed under the terms of the MIT license.
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,46 @@
-use session::Session;
+use clap::{
+    crate_authors, crate_description, crate_name, crate_version, value_t_or_exit, App, Arg,
+};
 
 use crate::config::TomatoConfig;
+use crate::session::Session;
 
 mod config;
 mod session;
 
 fn main() {
-    let config = TomatoConfig::new(25, 5, 15);
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(
+            Arg::with_name("work_time")
+                .long("work-time")
+                .short("w")
+                .default_value("25")
+                .help("Duration of work time (minutes)"),
+        )
+        .arg(
+            Arg::with_name("short_break_time")
+                .long("short-break-time")
+                .short("s")
+                .default_value("5")
+                .help("Duration of short break time (minutes)"),
+        )
+        .arg(
+            Arg::with_name("long_break_time")
+                .long("long-break-time")
+                .short("l")
+                .default_value("15")
+                .help("Duration of long break time (minutes)"),
+        )
+        .get_matches();
+
+    let work_time = value_t_or_exit!(matches.value_of("work_time"), u64);
+    let short_break_time = value_t_or_exit!(matches.value_of("short_break_time"), u64);
+    let long_break_time = value_t_or_exit!(matches.value_of("long_break_time"), u64);
+
+    let config = TomatoConfig::new(work_time, short_break_time, long_break_time);
     let mut session = Session::new(config);
     session.run();
 }
